059a66c45b953acfd492b839c5b9eba853b72f2a
Jakub Dudycz <dudyczjakub@gmail.com>
2018-11-25 23:03:11 +0100

    Dockerize project

--- coding-challenge-main/src/main/java/com/cs/main/Main.java
+++ coding-challenge-main/src/main/java/com/cs/main/Main.java
@@ -5,12 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.cs")
+@ComponentScan(basePackages = "com.cs")
+@EnableReactiveMongoRepositories(basePackages = "com.cs")
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
--- coding-challenge-main/src/main/java/com/cs/main/config/CorsConfiguration.java
+++ coding-challenge-main/src/main/java/com/cs/main/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.cs.main;
+package com.cs.main.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
